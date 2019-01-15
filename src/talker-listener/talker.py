ca22608f1e95132c0c6b044860847c4480512ba6
brunofreitasv <brunofreitas.v@hotmail.com>
2019-01-15 14:33:34 -0200

    New files and modifications

--- src/talker-listener/talker.py
+++ src/talker-listener/talker.py
@@ -1,3 +1,21 @@
-from __future__ import division
+#!/usr/bin/env python
 import rospy
-import std_msgs
+from std_msgs.msg import String
+
+def talker():
+    rospy.init_node('talker', anonymous = True)
+    pub = rospy.Publisher('chatter', String, queue_size = 10)
+    rate = rospy.Rate(1)
+    i = 0
+    while not rospy.is_shutdown():
+        hello_str = 'Hello, world %s'% i
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+        i += 1
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
